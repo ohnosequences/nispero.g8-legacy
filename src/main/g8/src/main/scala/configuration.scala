5cb9da8c90c40671a3cce89bee34b21e0b6e8d09
evdokim <museeer@gmail.com>
2013-09-12 10:31:54 +0200

    back to instanceProfile name

--- src/main/g8/src/main/scala/configuration.scala
+++ src/main/g8/src/main/scala/configuration.scala
@@ -23,7 +23,7 @@ case object configuration extends Configuration {
     amiId = AMI44939930.id,
     securityGroups = List("nispero"),
     keyName = "nispero",
-    instanceProfile = Some("$instanceProfileARN$")
+    instanceProfile = Some("nispero")
 
   )
 
